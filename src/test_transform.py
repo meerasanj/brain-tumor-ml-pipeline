c6769ffa0dc61a28ca8b7e58cdd5465cceb57f5f
Meera Sanjeevirao <meerasanj3@gmail.com>
2025-06-24 19:48:43 -0400

    update test_transform.py comments

--- src/test_transform.py
+++ src/test_transform.py
@@ -1,9 +1,11 @@
 from torchvision import transforms
 from src.config import Config
 
+# Method to create/return composition of image transformations for testing/validation data
+# Ensures that input images are consistently sized and normalized for model's expected format
 def get_test_transform():
     return transforms.Compose([
         transforms.Resize(Config.IMAGE_SIZE),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        transforms.ToTensor(), # PIL Image --> PyTorch Tensor 
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # standard ImageNet statistics
     ])
