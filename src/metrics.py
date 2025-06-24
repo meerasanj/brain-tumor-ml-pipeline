c5fce8fc890a718e679a98ba768c634f5e571eb7
Meera Sanjeevirao <meerasanj3@gmail.com>
2025-06-24 19:38:56 -0400

    update metrics.py comments

--- src/metrics.py
+++ src/metrics.py
@@ -1,17 +1,26 @@
-from sklearn.metrics import (
+# Imports in order to calculate metrics
+from sklearn.metrics import ( 
     classification_report,
     confusion_matrix,
     accuracy_score
 )
-import numpy as np
+import numpy as np # For array manipulation
 import logging
 
+# Metrics class is a utility class to group metrics-related calculations.
+# Can be used without needing to create an instance of the class
 class Metrics:
+    # Method to calculate a suite of classification metrics
+    # y_true (list): A list of the true, ground-truth labels.
+    # y_pred (list): A list of the labels predicted by the model.
     @staticmethod
     def calculate(y_true: list, y_pred: list) -> dict:
+        # Returns dictionary containing  accuracy score, classification report, and confusion matrix
         return {
+            # Proportion of correct predictions
             "accuracy": accuracy_score(y_true, y_pred),
-            "classification_report": classification_report(
+            # Precision, recall, and F1-score for each class
+            "classification_report": classification_report( 
                 y_true, y_pred, output_dict=True
             ),
             "confusion_matrix": confusion_matrix(y_true, y_pred).tolist()
