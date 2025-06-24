c83c2d79664d866b4a527c07f561b2eb37df945c
Meera Sanjeevirao <meerasanj3@gmail.com>
2025-06-24 19:53:52 -0400

    update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,5 @@ from .data_handler import DataHandler
 from .model import MedicalImageClassifier
 from .metrics import Metrics
 
+# __all__ var is a list of strings defining the public API of this package
 __all__ = ['Config', 'DataHandler', 'MedicalImageClassifier', 'Metrics']
